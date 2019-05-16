fe617859084f5567f1401987d572652294f4319f
liyuan3210 <li.yuan3210@163.com>
2019-05-16 10:42:30 +0800

    delete shell

--- shell/project/cron_push_git.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#数据文件log.txt定时提交到gitee包含写入的时间(定时需要配置crontab)
-time=$(date "+%Y-%m-%d %H:%M:%S")
-echo "call push time : ${time}">>/home/liyuan/tool/prv/log.txt
-cd /home/liyuan/tool/prv
-git pull
-git add *
-git commit -m "sys call time:${time}"
-git push
